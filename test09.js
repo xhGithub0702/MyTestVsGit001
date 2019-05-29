38086828c61398e382722c4db4a58c1625ecffbe
Fxh0527 <1462665631@qq.com>
2019-05-29 11:48:31 +0900

    git status

---
+++ test09.js
@@ -0,0 +1,10 @@
+var jsJson = function(){
+      return {
+            test1:function(){
+            	alert("test1");
+            },
+            test2:function(){
+            	alert("test2");
+	    }
+      };
+}();
